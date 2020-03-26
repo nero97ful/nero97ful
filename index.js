ee78fea8404ffd47becf00ef0b3e19e938e6211f
bertye <baert.ye@gmail.com>
2020-03-26 16:33:07 +0100

    added img hash package

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 const { imageHash }= require('image-hash');
+const imghash = require('imghash');
 const hamming = require('hamming-distance');
 require('dotenv').config();
 
-const promiseNewImg = new Promise((resolve, reject) => {
-  imageHash(process.env.IMG_A, 16, true, (error, data) => {
-    if (error) 
-      reject(error);
-    resolve(data);
-  });
-});
-
-const promiseCompImg = new Promise((resolve, reject) => {
-  imageHash(process.env.IMG_B, 16, true, (error, data) => {
-    if (error)
-      reject(error);
-    resolve(data); 
-  });
-});
+const promiseNewImg = imghash.hash(process.env.IMG_A, 16);
+const promiseCompImg = imghash.hash(process.env.IMG_B, 16);
 
 Promise.all([promiseNewImg, promiseCompImg]).then(([dataNewImg, dataCompImg]) => {
   console.log(dataNewImg);
